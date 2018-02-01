3f8b75a733f7ff809c3a15292a45d3ce6da94b1d
TESS <tessashley.webdev@gmail.com>
2018-02-01 00:20:29 -0600

    installs react-router-dom,wraps App component in BrowserRouter component, adds routes for / and /todos,uses Switch component to switch between routes, creates and renders home component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
 import React, { Component } from 'react';
+// create a Header component to show up across all of our app's pages
+import Header from './components/Header';
+// adds 2 routes for / and /todos
+import { Switch, Route } from 'react-router-dom';
+import Home from './components/Home';
+import TodosContainer from './containers/TodosContainer';
+//
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>Hello World</h1>
+        {/*Switch component from react-router-dom to tell our app to switch between different routes, depending on the URL*/}
+        <Switch>
+          {/* Route component, also given to us by react-router-dom to create a route for the root path('/'),
+           establish that the component that should be rendered here is a Home component */}
+          <Route exact path='/' component={ Home }/>
+          {/*There is a second route for the path /todos, which should route to a TodosContainer component*/}
+          <Route path='/todos' component={ TodosContainer }/>
+        </Switch>
       </div>
     );
   }
---
+++ src/components/Home.js
@@ -0,0 +1,15 @@
+//  simple React component
+// curly braces? only importing a specific module of the react-router-dom and name spacing it within Route 
+import React, { Component } from 'react';
+
+class Home extends Component {
+  render() {
+    return (
+      <h2>
+        this is home
+      </h2>
+    );
+  }
+}
+
+export default Home;
