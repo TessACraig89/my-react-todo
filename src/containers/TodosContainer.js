c99c2db2d4736464bcde7822a5b3b07bb25ee3fb
TESS <tessashley.webdev@gmail.com>
2018-02-01 00:48:42 -0600

    replaces dummy text in src/containers/TodosContainer.js

--- src/containers/TodosContainer.js
+++ src/containers/TodosContainer.js
@@ -1,15 +1,15 @@
 //  simple React component
 // curly braces? only importing a specific module of the react-router-dom and name spacing it within Route
-import React, { Component } from 'react';
+import React, {Component} from 'react'
 
-class Todo extends Component {
-  render() {
+class TodosContainer extends Component {
+  render(){
     return (
-      <h3>
-        todo
-      </h3>
-    );
+      <div className='todosContainer'>
+        <h2>This is the todos container</h2>
+      </div>
+    )
   }
 }
 
-export default Todo;
+export default TodosContainer
